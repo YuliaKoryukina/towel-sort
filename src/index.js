bc1ec938cf32516220847311e7d320abb3a8c0a5
Юлия Корюкина <koryuckina.yu@yandex.ru>
2026-05-09 20:29:34 +0200

    feat: implement towelSort algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  const result = matrix.map((row, index) => {
+    if (index % 2 !== 0) {
+      return row.reverse();
+    }
+    return row;
+  });
+  return result.flat();
 };
